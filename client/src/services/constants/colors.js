cfae01cdce7c808e778bff97acb7b96a476a2022
yabeye <yeabsera.work@gmail.com>
2022-06-02 00:29:44 +0300

    Folder setup.

---
+++ client/src/services/constants/colors.js
@@ -0,0 +1,10 @@
+// All colors used in this projects are specified here
+// to make the project consistent in look
+
+export const kPrimaryColorMain = '#2563eb'; //  tailwind - blue-600
+export const kPrimaryColorFade = '#60a5fa'; //  tailwind - blue-400
+
+export const kSecondaryColorMain = '#6b7280'; // tailwind - grey-500
+export const kSecondaryColorFade = '#d1d5db'; // tailwind - grey-300
+
+export const kDangerColor = '';
