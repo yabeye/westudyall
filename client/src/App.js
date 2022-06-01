bdc81d2e681431d1b67796a812f632217e0da3da
yabeye <yeabsera.work@gmail.com>
2022-06-01 09:27:58 +0300

    Initial commit.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="bg-blue-600 hover:bg-blue-400 hover:scale-110 duration-200 text-white py-2 px-3 m-5 rounded-full">
+        Start the Project
+      </button>
+    </div>
+  );
+}
+
+export default App;
